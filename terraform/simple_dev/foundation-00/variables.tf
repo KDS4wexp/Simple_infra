ae22f151dfabf0c96a5f8191a9854c37a72ce417
KDS4wexp <KDS4wexp@gmail.com>
2025-06-01 20:17:35 +0300

    fix: Added variables

--- terraform/simple_dev/foundation-00/variables.tf
+++ terraform/simple_dev/foundation-00/variables.tf
@@ -1,3 +1,15 @@
+variable "token-id" {
+    type = string
+} 
+
+variable "cloud-id"{
+    type = string
+}
+
+variable "folder-id"{
+    type = string
+}
+
 variable "public_ssh_key" {
   type = string
 }
